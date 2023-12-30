e90c5bab7317dc7e40314944c190fd38b09f0fc4
Chillmore <134298733+Chillmore69@users.noreply.github.com>
2023-12-30 11:29:39 +0330

    Delete includes/admin directory

--- includes/admin/category-delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_GET['category_id'])) {
-    Semej::set('danger', 'error', 'Missing user id');
-    header('Location: dashboard.php?page=categories');
-}
-$id = Sanitizer::sanitize($_GET['category_id']);
-
-$category = new Category();
-$category->delete($id);
--- includes/admin/post-delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_GET['post_id'])) {
-    Semej::set('danger', 'error', 'Missing Post id');
-    header('Location: dashboard.php?page=posts');
-}
-$id = Sanitizer::sanitize($_GET['post_id']);
-
-$post = new Post();
-$post->delete($id);
